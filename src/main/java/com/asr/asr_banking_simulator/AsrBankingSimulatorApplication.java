f7b139bb2517a03ef55885f20d3d26807fa1d5b4
SSDLinux <shubhamdeshmukhs741@gmail.com>
2026-09-24 07:58:15 +0530

    Initialized Project

---
+++ src/main/java/com/asr/asr_banking_simulator/AsrBankingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.asr.asr_banking_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrBankingSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsrBankingSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asr/asr_banking_simulator/AsrBankingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asr.asr_banking_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrBankingSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
